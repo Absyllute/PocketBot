db4b7bfd2c1097b4eb2eef841ad9869b05e66eb8
Λbsyllute <absyllute@gmail.com>
2026-08-29 15:19:53 +0200

    feat (core): Log bot in with token in .env

--- src/main.py
+++ src/main.py
@@ -6,3 +6,13 @@ load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 DEV_GUILD_ID = int(str(getenv("DEV_GUILD_ID"))) # PyCharm is really strict with types for some reason
 dev_guild = discord.Object(id=DEV_GUILD_ID)
+
+customIntents = discord.Intents.default()
+customIntents.message_content = True
+
+class BotClient(discord.Client):
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+
+client = BotClient(intents=customIntents)
+client.run(TOKEN)
