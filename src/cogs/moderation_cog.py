2fe4a5d6da50f3cf4141300d0e319b427ea254db
Λbsyllute <absyllute@gmail.com>
2026-08-30 07:22:38 +0200

    feat: added purge-human command

--- src/cogs/moderation_cog.py
+++ src/cogs/moderation_cog.py
@@ -6,7 +6,7 @@ class ModerationCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="purge", description="Mass delete a configurable amount of messages")
+    @app_commands.command(name="purge-any", description="Mass delete a configurable amount of messages")
     async def purge(self, interaction: discord.Interaction, amount: int):
         await interaction.response.defer(ephemeral=True)
 
@@ -16,6 +16,20 @@ class ModerationCog(commands.Cog):
             await interaction.followup.send(f"Deleted {len(deleted)} messages!")
         else:
             await interaction.followup.send("Unable to purge messages in this channel", ephemeral=True)
+
+    @app_commands.command(name="purge-human", description="Mass delete a configurable amount of messages made by humans (non-nots)")
+    async def human_purge(self, interaction: discord.Interaction, amount: int):
+        await interaction.response.defer(ephemeral=True)
+
+        if isinstance(interaction.channel, (discord.TextChannel, discord.VoiceChannel)):
+            def is_human(msg: discord.Message) -> bool:
+                return not msg.author.bot
+
+            messages_deleted = await interaction.channel.purge(limit=amount, check=is_human)
+
+            await interaction.followup.send(f"Deleted {len(messages_deleted)} human messages! (bots skipped)", ephemeral=True)
+        else:
+            await interaction.followup.send("Unable to purge messages in this channel")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(ModerationCog(bot=bot))
