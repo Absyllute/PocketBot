26226dec261a3e0172017a99020445410379be3a
Λbsyllute <absyllute@gmail.com>
2026-08-31 18:17:24 +0200

    User QoL: improved wording in labels and placeholers in the EmbedModal

--- src/modals/embed_builder_modal_.py
+++ src/modals/embed_builder_modal_.py
@@ -4,23 +4,23 @@ import discord
 class EmbedBuilderModal(ui.Modal, title="Create a custom embed"):
 
     embed_title = ui.TextInput(
-        label="Embed Title",
-        placeholder="Title, Keep it short and descriptive",
+        label="Embed title",
+        placeholder="Enter announcement title here...",
         required=True,
         max_length=64
     )
 
     embed_desc = ui.TextInput(
-        label="Content / Description",
-        placeholder="Hello World! The content of your embed goes here. Markdown supported :)",
+        label="Description (Markdown supported)",
+        placeholder="Type your message here...",
         required=True,
         max_length=1024,
         style=TextStyle.paragraph
     )
 
     embed_thumb = ui.TextInput(
-        label="Image URL for the embed thumbnail (Optional)",
-        placeholder="https://example.com/image.png",
+        label="Thumbnail URL (Optional)",
+        placeholder="Image link here...",
         required=False
     )
 
