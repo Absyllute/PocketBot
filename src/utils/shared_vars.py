bc6e62563dc625e3846b3092f928a41f43c23ee4
Λbsyllute <absyllute@gmail.com>
2026-09-06 06:42:17 +0200

    dev: added is_mod_or_admin() check in shared_vars for later use

--- src/utils/shared_vars.py
+++ src/utils/shared_vars.py
@@ -1,5 +1,30 @@
 # This file is used to store commonly used global / shared variables
 # "wah wah wah, this isnt best practice" - kindly stfu as your opinion was not needed :heart: ~Absyllute
 
+from discord import Interaction, Member, app_commands
+### --- Utils --- ###
 smp_link = "play.pocketcraft-smp.online"
+
+class Config:
+    mod_roles: list[int] = [] # "Hello everyone, this is your daily dose of type safety" ~Probably vit12
+
+    @staticmethod
+    def is_mod_or_admin():
+        def predicate(interaction: Interaction) -> bool:
+            if not isinstance (interaction.user, Member):
+                return False
+
+            if interaction.user.guild_permissions.administrator:
+                return True
+
+            user_roles = [role.id for role in interaction.user.roles]
+
+            for role_id in user_roles:
+                if role_id in Config.mod_roles:
+                    return True
+
+            return False
+        return app_commands.check(predicate=predicate)
+
+### --- Styling --- ###
 primary_colour = 0x73d01e
