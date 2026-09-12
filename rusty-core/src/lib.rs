ddcc7e8159c42de973cf8092668d28b70951bd45
Λbsyllute <absyllute@gmail.com>
2026-09-12 13:30:16 +0200

    dev: added rusqlite dep and an init_db function

--- rusty-core/src/lib.rs
+++ rusty-core/src/lib.rs
@@ -4,6 +4,19 @@ use pyo3::prelude::*;
 #[pymodule]
 mod rusty_core {
     use pyo3::prelude::*;
+    use rusqlite::Connection;
+
+    #[pyfunction]
+    fn init_db() {
+        let db_conn = Connection::open("test.db").unwrap();
+
+        db_conn.execute("
+            CREATE TABLE IF NOT EXISTS modrole (
+                guild_id INTEGER PRIMARY KEY NOT NULL
+                role_ids INTERGER
+            )
+        ", []).unwrap(); // Not permament will be changed in the soon...
+    }
 
     #[pyfunction]
     fn test_fn() {
