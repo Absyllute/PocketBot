e5e80eae6ad0616c2a8a6671b885c9e2c9b8af89
Λbsyllute <absyllute@gmail.com>
2026-09-12 16:13:29 +0200

    feat(backend): added add_modrole function

--- rusty-core/src/lib.rs
+++ rusty-core/src/lib.rs
@@ -4,12 +4,14 @@ use pyo3::prelude::*;
 #[pymodule]
 mod rusty_core {
     use pyo3::prelude::*;
-    use rusqlite::Connection;
+    use rusqlite::{Connection};
+
+    const DATABASE: &str = "test.db";
 
     /// init_db creates the database if it doesnt exist.
     #[pyfunction]
     fn init_db() {
-        let db_conn = Connection::open("test.db").unwrap();
+        let db_conn = Connection::open(DATABASE).unwrap();
 
         db_conn.execute("
             CREATE TABLE IF NOT EXISTS modrole (
@@ -19,6 +21,18 @@ mod rusty_core {
         ", []).unwrap();
 
         println!("Rust: Initalised Databse")
+    }
+
+    #[pyfunction]
+    fn add_modrole(guild_id: i64, role_ids: Vec<i64>) {
+        let db_conn = Connection::open(DATABASE).unwrap();
+
+        let json_roles = serde_json::to_string(&role_ids).unwrap();
+
+        db_conn.execute("
+            INSERT INTO modrole (guild_id, role_id) VALUES (?1, ?2)
+            ON CONFLICT (guild_id) DO UPDATE SET role_ids = excluded.role_ids
+        ", rusqlite::params![guild_id, json_roles]).unwrap();
     }
 
     #[pyfunction]
