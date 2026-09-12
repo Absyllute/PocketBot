6a6a0adc22adb592eaac0f1bd41559c7bb21f3c7
Λbsyllute <absyllute@gmail.com>
2026-09-12 17:57:58 +0200

    feat(backend): added remove_modrole function and renemed role_id in the db to the singular

--- rusty-core/src/lib.rs
+++ rusty-core/src/lib.rs
@@ -4,7 +4,7 @@ use pyo3::prelude::*;
 #[pymodule]
 mod rusty_core {
     use pyo3::prelude::*;
-    use rusqlite::{Connection};
+    use rusqlite::{Connection, params};
 
     const DATABASE: &str = "test.db";
 
@@ -16,7 +16,7 @@ mod rusty_core {
         db_conn.execute("
             CREATE TABLE IF NOT EXISTS modrole (
                 guild_id INTEGER PRIMARY KEY NOT NULL,
-                role_ids TEXT
+                role_id  TEXT
             )
         ", []).unwrap();
 
@@ -30,9 +30,18 @@ mod rusty_core {
         let json_roles = serde_json::to_string(&role_ids).unwrap();
 
         db_conn.execute("
-            INSERT INTO modrole (guild_id, role_ids) VALUES (?1, ?2)
-            ON CONFLICT (guild_id) DO UPDATE SET role_ids = excluded.role_ids
+            INSERT INTO modrole (guild_id, role_id) VALUES (?1, ?2)
+            ON CONFLICT (guild_id) DO UPDATE SET role_id = excluded.role_id
         ", rusqlite::params![guild_id, json_roles]).unwrap();
+    }
+
+    #[pyfunction]
+    fn remove_modrole(guild_id: i64, role_id: i64) {
+        let db_conn = Connection::open(DATABASE).unwrap();
+
+        db_conn.execute("
+            DELETE FROM modrole WHERE guild_id = ?1 AND role_id = ?2
+        ", params![guild_id, role_id.to_string()]).unwrap();
     }
 
     #[pyfunction]
